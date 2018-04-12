b2fcb6726313fb4eb335d2ec44c2c8d8405cc6f5
pchuck77 <37907436+pchuck77@users.noreply.github.com>
2018-04-12 14:36:47 +0300

    follower

---
+++ 08/follower/Assets/Scripts/Motion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	public Transform[] objects;
+	// Update is called once per frame
+	void Update ()
+	{
+		int minimalRangeNumber=0;
+		float minimalRange = Vector3.Distance (objects [0].position, transform.position);
+		for (int i = 1; i < objects.Length; i++) {
+			float range = Vector3.Distance (objects [i].position, transform.position);
+			if (minimalRange > range) {
+				minimalRange = range;
+				minimalRangeNumber = i;
+
+			}
+		}
+		Vector3 direction = objects [minimalRangeNumber].position - transform.position;
+		direction.Normalize();
+		transform.Translate (direction * Time.deltaTime * 1);
+	}
+}

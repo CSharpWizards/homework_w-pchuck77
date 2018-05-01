c997581560f5d3e087d90ec3667f8f9bfa03d3c4
Alex <iseldomused@gmail.com>
2018-05-02 01:30:04 +0300

    add 10

---
+++ 10/AttackCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AttackCalculator {
+	class Program {
+		static void Main(string[] args) {
+			double attack1 = GetReducedAttack(50, 100, 20); // 10
+			double attack2 = GetReducedAttack(20, 80, 60);  // 15
+			Console.WriteLine("Сила атаки монстра 1: " + attack1);
+			Console.WriteLine("Сила атаки монстра 2: " + attack2);
+
+			Console.ReadLine();
+		}
+
+		static double GetReducedAttack(double health, double maxHealth, double maxAttack) {
+		}
+	}
+}

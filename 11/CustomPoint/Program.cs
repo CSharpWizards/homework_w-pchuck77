ea229d6ec6f08ff9acb7ce88fd5e86b4868ee784
Alex <iseldomused@gmail.com>
2018-05-09 14:39:16 +0300

    add 11

---
+++ 11/CustomPoint/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPoint {
+	class Program {
+		static void Main(string[] args) {
+			Point point1 = new Point();
+			point1.x = 2;
+			point1.y = 5;
+			point1.symbol = "C";
+			point1.Draw();
+
+			Point point2 = new Point();
+			point2.x = 6;
+			point2.y = 3;
+			point2.symbol = "#";
+			point2.Draw();
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ 11/CustomRectangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRectangle {
+	class Program {
+		static void Main(string[] args) {
+			Rectangle rect1 = new Rectangle();
+			rect1.x = 2;
+			rect1.y = 2;
+			rect1.width = 3;
+			rect1.height = 3;
+			rect1.symbol = "C";
+			rect1.Draw();
+
+			Rectangle rect2 = new Rectangle();
+			rect2.x = 7;
+			rect2.y = 2;
+			rect2.width = 5;
+			rect2.height = 4;
+			rect2.symbol = "#";
+			rect2.Draw();
+
+			Console.ReadLine();
+		}
+	}
+}
